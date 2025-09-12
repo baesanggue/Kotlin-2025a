3be0e7e79a8b8a6e7007b60f79c94786b3a053ea
baesanggue <sam421862@gmail.com>
2025-09-12 11:56:06 +0900

    week02 : variables02 (val, var)

--- KotilnBasics/app/src/main/java/com/kotilnbasics/MainActivity.kt
+++ KotilnBasics/app/src/main/java/com/kotilnbasics/MainActivity.kt
@@ -32,17 +32,35 @@ class MainActivity : ComponentActivity() {
 }
 
 private fun week02Variables(){
-    println("Week 02 : Variables")
+//    println("Week 02 : Variables")
+//
+//    val courseName = "Mobile Programming"
+//    //courseName = "IoT Programming"
+//    //val 로 선언한건 재할당이 안됨
+//
+//    var week = 1
+//    week = 2
+//    // var로 선언한건 재할당 가능
+//    println("Course : $courseName")
+//    println("Week : $week")
+    println("Kotlin Variables ==")
 
-    val courseName = "Mobile Programming"
-    //courseName = "IoT Programming"
-    //val 로 선언한건 재할당이 안됨
+    // val(Immutable: 불변성) vs var(mutable: 가변성)
+    val name = "Android"
+    var version = 8
 
-    var week = 1
-    week = 2
-    // var로 선언한건 재할당 가능
-    println("Course : $courseName")
-    println("Week : $week")
+    println("Hello $name $version")
+
+    val age: Int = 26
+    val height: Double = 175.8
+    val isStudent: Boolean = true
+
+    println("Age : $age, Height: $height, Student : $isStudent")
+
+   // var nickname: String = null  컴파일시 에러 잡음 널 허용이 안되는 스트링
+    var nickname: String? = null // 얘는 널 허용되는 스트링이라 컴파일시 오류 안나옴
+    nickname = "mirae"
+    println("Nickname: $nickname, NicknameLength:  ${nickname?.length}")
 }
 
 @Composable
