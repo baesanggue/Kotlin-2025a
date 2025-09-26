33d3f37f66ad36d1d49b955064caea4bfd75ce91
sanggue <ecc0308@dongyang.ac.kr>
2025-09-26 11:28:58 +0900

    week04: Greeting App v0.1

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.appweek04
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //enableEdgeToEdge() 자동생성 제거
+        setContentView(R.layout.activity_main) //뷰 연동 코드
+
+        val editTextName = findViewById<EditText>(R.id.editTextName) // 입력상자랑 변수 바인딩. 불변으로 선언
+        val buttonGreet = findViewById<Button>(R.id.buttonGreet)
+        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+
+        buttonGreet.setOnClickListener {
+            val name = editTextName.text.toString().trim()
+
+            var greeting: String =""
+
+            if (name.isNotEmpty()) {
+                greeting = "안녕하세요, ${name}님!"
+
+            }
+            else {
+                greeting = "이름을 입력해주세요"
+            }
+            textViewGreeting.text = greeting
+            textViewGreeting.visibility = View.VISIBLE
+            Log.d("KotlinWeek04App", greeting)
+        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets  안쓰니까 지워
         }
     }
-}
